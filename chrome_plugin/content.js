d1bd83fda15de705a61cffd651d005363848ad6b
me3eh <matt30002000@gmail.com>
2024-05-28 18:23:45 +0200

    Add little improvements to code - now it works with python server

--- chrome_plugin/content.js
+++ chrome_plugin/content.js
@@ -1,21 +1,22 @@
 elements = [];
+function addStyleString(str) {
+  var node = document.createElement('style');
+  node.innerHTML = str;
+  document.body.appendChild(node);
+}
+addStyleString('.-sitemap-select-item-hover{outline:2px solid red!important}.-sitemap-select-item-hover,.-sitemap-select-item-hover *{background-color:rgba(0,213,0,.2)!important;background:rgba(0,213,0,.2)!important}.-sitemap-select-item-selected{outline:2px solid #c70000!important}.-sitemap-select-item-selected,.-sitemap-select-item-selected *{background-color:rgba(213,0,0,.2)!important;background:rgba(213,0,0,.2)!important}')
+// addStyleString('.-sitemap-select-item-hover2{outline:2px solid green!important}.-sitemap-select-item-hover,.-sitemap-select-item-hover *{background-color:rgba(0,213,0,.2)!important;background:rgba(0,213,0,.2)!important}.-sitemap-select-item-selected{outline:2px solid red!important}.-sitemap-select-item-selected,.-sitemap-select-item-selected *{background-color:rgba(213,0,0,.2)!important;background:rgba(213,0,0,.2)!important}')
+addStyleString('.-false-news{outline:20px solid red!important}')
+addStyleString('.-real-news{outline:20px solid green!important}')
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'convert') {
     // document.documentElement.style.filter = 'grayscale(100%)';
     // alert("heh");
-  function addStyleString(str) {
-      var node = document.createElement('style');
-      node.innerHTML = str;
-      document.body.appendChild(node);
-  }
   
-  addStyleString('.-sitemap-select-item-hover{outline:2px solid green!important}.-sitemap-select-item-hover,.-sitemap-select-item-hover *{background-color:rgba(0,213,0,.2)!important;background:rgba(0,213,0,.2)!important}.-sitemap-select-item-selected{outline:2px solid #c70000!important}.-sitemap-select-item-selected,.-sitemap-select-item-selected *{background-color:rgba(213,0,0,.2)!important;background:rgba(213,0,0,.2)!important}')
-  // addStyleString('.-sitemap-select-item-hover2{outline:2px solid green!important}.-sitemap-select-item-hover,.-sitemap-select-item-hover *{background-color:rgba(0,213,0,.2)!important;background:rgba(0,213,0,.2)!important}.-sitemap-select-item-selected{outline:2px solid red!important}.-sitemap-select-item-selected,.-sitemap-select-item-selected *{background-color:rgba(213,0,0,.2)!important;background:rgba(213,0,0,.2)!important}')
-  addStyleString('.-sitemap-select-item-hover2{outline:2px solid red!important}.-sitemap-select-item-hover,.-sitemap-select-item-hover *{background-color:rgba(255,0,0,.2)!important;background:rgba(255,0,0,.2)!important}.-sitemap-select-item-selected{outline:2px solid darkred!important}.-sitemap-select-item-selected,.-sitemap-select-item-selected *{background-color:rgba(139,0,0,.2)!important;background:rgba(139,0,0,.2)!important}')
-
   async function sendDetectionRequest(data) {
-    const json = `{"data": "${data}"}`;
-    const obj = JSON.parse(json);
+    const json = {data: `${data}`};
+    const obj = JSON.stringify(json);
     const response = await fetch('http://127.0.0.1:5000/detection', {
         method: 'POST',
         headers: {
@@ -38,23 +39,32 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   function mouseout(event) {
       event.target.classList.remove('-sitemap-select-item-hover')
   }
-  function clicking(event){
+  async function clicking(event){
       event.preventDefault();
       element = document.elementFromPoint(event.clientX, event.clientY);
       elements.push(element);
       
-      // alert(element.innerText);
-      
-      let xD = sendDetectionRequest('some plain text data');
-      alert(xD);
-
+      let result ="dupa";
+      try {
+        const response = await sendDetectionRequest(element.innerText);
+        result = response.result;
+      } catch (error) {
+        let dupa = "dupa";
+      }
+      alert(result);
+      if( result == "false"){
+        element.classList.add("-real-news")
+      }
+      else{
+        element.classList.add("-false-news")
+      }
       document.removeEventListener("mouseout", mouseout);
       document.removeEventListener("mouseover", mouseover);
       document.removeEventListener("click", clicking);
   }
   
-  lol = document.addEventListener('mouseover', mouseover)
-  kekw = document.addEventListener('mouseout', mouseout)
+  document.addEventListener('mouseover', mouseover)
+  document.addEventListener('mouseout', mouseout)
   document.addEventListener('click', clicking);
 
   sendResponse({ message: 'Website converted to black and white.' });
